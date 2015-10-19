663febca7d2efbbff4fa55ba25def1832f5bcdb7
AndrewSanderson <1306507@rgu.ac.uk>
2015-10-19 15:53:15 +0100

    It is safe.

--- echo.php
+++ echo.php
@@ -6,15 +6,48 @@
  * Time: 14:55
  */
 
-$provisionedActivity = array("specs", "drugs", "rocknroll"); // declares the array
-
-$provisionedActivity[1] =  "hugs";
-
-unset($provisionedActivity[2]); // removes the array in position 2
-
-foreach($provisionedActivity as $x) {
-    print "<p>$x</p>";
+switch(date("d")){
+    case 1:
+    case 5:
+    case 9:
+    case 13:
+    case 17:
+    case 21:
+    case 25:
+    case 29:
+        echo "No products are avaliable";
+        break;
+    case 2:
+    case 6:
+    case 10:
+    case 14:
+    case 18:
+    case 22:
+    case 26:
+    case 30:
+        echo "Specs are avaliable";
+        break;
+    case 3:
+    case 7:
+    case 11:
+    case 15:
+    case 19:
+    case 23:
+    case 27:
+    case 31:
+        echo "Mugs are avaliable";
+        break;
+    case 4:
+    case 8:
+    case 12:
+    case 16:
+    case 20:
+    case 24:
+    case 28:
+        echo "Specs and sausage rolls are avaliable";
+        break;
 }
+
 
 echo date("d");
 
