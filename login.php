bfcb13030b816f15ebe07e4774c725296b498bdf
turkishlampost <michael.everett@outlook.com>
2015-12-14 22:47:54 +0000

    Testing login

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ if (isset($_POST['submit'])) {
         $username=$_POST['username'];
         $password=$_POST['password'];
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-//// Selecting Database
+// Selecting Database
         $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
 // To protect MySQL injection for Security purpose
         $username = stripslashes($username);
