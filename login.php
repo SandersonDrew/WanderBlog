7ddce65361bfcd4b6a88e7cdc029540fa4c16f41
turkishlampost <michael.everett@outlook.com>
2015-12-14 21:11:26 +0000

    Testing login

--- login.php
+++ login.php
@@ -11,15 +11,14 @@ if (isset($_POST['submit'])) {
         $username=$_POST['username'];
         $password=$_POST['password'];
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-        $connection = mysql_connect("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7");
+//// Selecting Database
+        $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
 // To protect MySQL injection for Security purpose
         $username = stripslashes($username);
         $password = stripslashes($password);
         $username = mysql_real_escape_string($username);
         $password = mysql_real_escape_string($password);
-// Selecting Database
-        $db = mysql_select_db("wb1306507", $connection);
-// SQL query to fetch information of registerd users and finds user match.
+// SQL query to fetch information of registered users and finds user match.
         $query = mysql_query("select * from login where password='$password' AND username='$username'", $connection);
         $rows = mysql_num_rows($query);
         if ($rows == 1) {
