c09ff2dcfa344d29ae24ccfb7f922256045d63a5
AndrewSanderson <1306507@rgu.ac.uk>
2015-12-15 22:17:01 +0000

    Adeventure

--- adventure.php
+++ adventure.php
@@ -2,6 +2,7 @@
 echo "lol";
 echo "Banter";
 $fi = new FilesystemIterator("/photos/dan/1", FilesystemIterator::SKIP_DOTS);
+echo "Medoicre Banter";
 echo iterator_count($fi);
 echo "greate banter";
 ?>
