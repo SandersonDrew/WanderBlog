28642074827e234a622109695876dd49b2e90569
1301823 <michael.everett@outlook.com>
2015-12-15 16:26:38 +0000

    Login stuff

--- updateSettings.php
+++ updateSettings.php
@@ -5,14 +5,18 @@ echo $userid;
 if (isset($_POST['submit'])) {
     if (empty($_POST['username']) || empty($_POST['password'])) {
         $error = "Username or Password is empty";
+
     } else {
         $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
+        if ($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+        }
+        echo "Connected successfully";
         $newUsername = $_POST['username'];
 //        $password = $_POST['password'];
 //        $email = $_POST['email'];
-        echo "Before sql";
         $query = mysqli_query($connection, "UPDATE users SET username='$newUsername' WHERE userid='$userid'");
-        echo "After sql";
+
         $result = mysqli_num_rows($query);
         if ($result == 1) {
             $_SESSION['login_user']=$newUsername; // Initializing Session
