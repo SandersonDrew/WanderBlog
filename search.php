77a0d518de978f041d1a1d1be48e958236cfdfb5
1306507 <1306507@rgu.ac.uk>
2015-12-15 13:47:46 +0000

    It is safe.

--- search.php
+++ search.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "username: " . $row["username"]."<br>";
+        echo "<br>". "username: " . $row["username"]."</br>";
     }
 } else {
     echo "0 results";
