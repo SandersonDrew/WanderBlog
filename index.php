67f94d3180b6b846906d5075eea59a4e079731a1
1301823 <michael.everett@outlook.com>
2015-12-15 12:59:41 +0000

    Login stuff

--- index.php
+++ index.php
@@ -12,7 +12,20 @@ if(isset($_SESSION['login_user'])){
     <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-
+<div id="main">
+    <h1>WanderBlog Login</h1>
+    <div id="login">
+        <h2>Login </h2>
+        <form action="login.php" method="post">
+            <label>UserName :</label>
+            <input id="name" name="username" placeholder="username" type="text">
+            <label>Password :</label>
+            <input id="password" name="password" placeholder="**********" type="password">
+            <input name="submit" type="submit" value=" Login ">
+            <span><?php echo $error; ?></span>
+        </form>
+    </div>
+</div>
 <div id="main2">
     <div id="NewUser">
         <h2>New User</h2>
@@ -28,3 +41,4 @@ if(isset($_SESSION['login_user'])){
 </div>
 </body>
 </html>
+INSERT INTO users VALUES ('2','Fred','bacon',0)
