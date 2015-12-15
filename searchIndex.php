07c3ebfe39e2168d2a8c945ec55f0e3e712207b9
1306507 <1306507@rgu.ac.uk>
2015-12-15 14:19:57 +0000

    It is safe.

--- searchIndex.php
+++ searchIndex.php
@@ -4,10 +4,10 @@
     <title> My search engine </title>
 </head>
 <body>
-< form action = 'search.php' method = 'GET' >
+<form action = 'search.php' method = 'GET' >
 < center >
 <h1 > My Search Engine </h1 >
-< input type = 'text' size='90' name = 'search' >
+<input type = 'text' size='90' name = 'search' >
 </br >
 </br >
 <input type = 'submit' name = 'submit' value = 'Search source code' >
