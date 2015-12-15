a4702bb533a9f0d7b57b470aed412257529d0ce0
1308353 <1308353@rgu.ac.uk>
2015-12-15 14:15:52 +0000

    Profile page edits - Body

--- profile.php
+++ profile.php
@@ -60,7 +60,7 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-6">
         <p id="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
             Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
             Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
