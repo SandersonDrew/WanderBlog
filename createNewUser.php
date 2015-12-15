837d0cbf84e2e2f3cacfe8e9fd1916cbdf91cc04
1301823 <michael.everett@outlook.com>
2015-12-15 14:41:06 +0000

    Login stuff

--- createNewUser.php
+++ createNewUser.php
@@ -2,14 +2,15 @@
 session_start(); // Starting Session
 $error=''; // Variable To Store Error Message
 if (isset($_POST['submit'])) {
-    if (empty($_POST['username']) || empty($_POST['password'])) {
-        $error = "Username or Password is empty";
+    if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
+        $error = "Username, Password or Email is empty";
     }
     else
     {
 // Define $username and $password
         $username=$_POST['username'];
         $password=$_POST['password'];
+        $email = $_POST['email'];
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
 // Selecting Database
         $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
@@ -22,7 +23,7 @@ if (isset($_POST['submit'])) {
         $query = mysqli_query($connection,"SELECT * FROM users WHERE username='$username'");
         $result = mysqli_num_rows($query);
         if ($result == 0) {
-            mysqli_query($connection, "INSERT INTO users(username,password,permissionLevel,verified) VALUES('$username', '$password', '0','FALSE') ");
+            mysqli_query($connection, "INSERT INTO users(username,password,permissionLevel,verified,email) VALUES('$username', '$password', '0','FALSE','$email') ");
             $_SESSION['login_user']=$username; // Initializing Session
             header("location: profiletest.php"); // Redirecting To Other Page
         } else {
