724568b156cec2e2d073c972c6f8186d08d5d1ff
1306507 <1306507@rgu.ac.uk>
2015-12-15 13:49:23 +0000

    It is safe.

--- search.php
+++ search.php
@@ -9,7 +9,7 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully<br>";
 
 $sql = "SELECT username FROM users";
 $result = $conn->query($sql);
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br>". "username: " . $row["username"]."</br>";
+        echo "username: " . $row["username"]."<br>";
     }
 } else {
     echo "0 results";
