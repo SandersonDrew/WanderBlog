80c3c41db56ae58b4f90039336d7e73aaa7b3a24
1308353 <1308353@rgu.ac.uk>
2015-12-15 12:02:22 +0000

    Profile page edits

--- profile.php
+++ profile.php
@@ -26,7 +26,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php"><img src="Photos\wlogo.png" alt="logo" style="width:150px;height:75px;"</a>
+                <a class="navbar-brand" href="index.php"><img src="Photos\wlogo.png" alt="logo" style="width:150px;height:50px;"</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
