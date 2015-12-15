73d13886476e53c85d5e070855d55133ca8368a9
1301823 <michael.everett@outlook.com>
2015-12-15 11:28:18 +0000

    Login stuff

---
+++ profiletest.php
@@ -0,0 +1,16 @@
+<?php
+include('session.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Home Page</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="profile">
+    <b id="welcome">Welcome : <i><?php echo $_SESSION['login_user']; ?></i></b>
+    <b id="logout"><a href="logout.php">Log Out</a></b>
+</div>
+</body>
+</html>
