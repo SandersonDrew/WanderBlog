6d4d6be7231337c2f4563330c918c0c28ce72660
1301823 <michael.everett@outlook.com>
2015-12-15 11:56:43 +0000

    Login stuff

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include('login.php'); // Includes Login Script
 
 if(isset($_SESSION['login_user'])){
-    header("location: profile.php");
+    header("location: profiletest.php");
 }
 ?>
 <!DOCTYPE html>
