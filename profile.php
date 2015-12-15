41df9bd7a63467c38a3fdbeab8b1c88345a6c6f8
1308353 <1308353@rgu.ac.uk>
2015-12-15 14:20:26 +0000

    Profile page edits - Body

--- profile.php
+++ profile.php
@@ -67,14 +67,13 @@
 <!--    </div>-->
 <!--</div>-->
 
-<div id="desc" class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <p >Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+
+            <p id="desc" align="center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
                 Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
                 Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
-        </div>
-    </div>
-</div>
+
+
+
+
 </body>
 </html>
