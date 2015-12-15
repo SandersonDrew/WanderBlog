4e1ef4bbf296b28780cb9c18b4a605d86cb37299
1308353 <1308353@rgu.ac.uk>
2015-12-15 14:07:34 +0000

    Profile page edits - Body

--- profile.php
+++ profile.php
@@ -55,6 +55,9 @@
         <div class="span4"></div>
         <div class="span4"><img class="center-block img-circle" src="http://placehold.it/150x50&text=Logo"  alt="Profile-Photo" ></div>
         <div class="span4"><h1 id="userName">Name</h1></div>
+        <div class="span4"><p id="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+                              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec,
+                              pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p></div>
         <div class="span4"></div>
     </div>
 </div>
