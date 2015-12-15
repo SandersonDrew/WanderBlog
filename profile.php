28d287aec9bdec6350110b3d2996209875fe67ab
1308353 <1308353@rgu.ac.uk>
2015-12-15 11:35:16 +0000

    Profile page edits

--- profile.php
+++ profile.php
@@ -16,7 +16,7 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <nav class="navbar navbar-inverse">
+    <nav class="navbar navbar-default">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
