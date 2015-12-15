e702530b06ad2d33f9f278ed5b4e2dbf71edafe3
1308353 <1308353@rgu.ac.uk>
2015-12-15 17:14:41 +0000

    Profile page edits - Body

--- profile.php
+++ profile.php
@@ -96,9 +96,7 @@ $userid = $_SESSION['userid'];
 <div id="desc" class="container">
     <div class="row">
         <div class="col-md-12">
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
-                Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
+            <p>check </p>
         </div>
     </div>
 </div>
