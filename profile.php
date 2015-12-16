15c1ce30a6b24ee9ca947f0fd36f89b4908fa04d
1301823 <michael.everett@outlook.com>
2015-12-16 16:59:10 +0000

    Create New Adventure

--- profile.php
+++ profile.php
@@ -47,7 +47,7 @@ if ($connection->connect_error) {
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">Logo goes here</a>
+                <a class="navbar-brand" href="index.php"><img  src="/Photos/wlogo.png"  alt="Profile-Photo" ></a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
