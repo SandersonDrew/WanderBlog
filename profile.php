910167dc6cc337812a2bdd6f36a6c0fbf4bb0f10
turkishlampost <michael.everett@outlook.com>
2015-12-16 22:56:16 +0000

    Stuff

--- profile.php
+++ profile.php
@@ -151,7 +151,8 @@ function genDivs()
                         <img  src="http://placehold.it/150x50&text=Logo"  alt="Profile-Photo" >
                         <h6> ' . $row['description']. ' </h6>
                         <form action="adventure.php" method="get">
-                            <input type="submit" name="adventureid" value="'.$row['adventureid'].'";
+                            <input type="hidden" name="adventureid" value="'.$row['adventureid'].'"/>
+                            <input type="submit" name="submit" value="Go To Adventure Page"/>
                         </form>
                     </div>
                     <div class="col-md-2"></div>
