e5e284939b849ad0479e4e42ed89878a34e6a31a
1304926 <1304926@rgu.ac.uk>
2015-12-16 13:19:52 +0000

    a

--- brianslonelypage.php
+++ brianslonelypage.php
@@ -4,8 +4,27 @@ if(isset($_SESSION['login_user'])){
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Settings Page</title>
+
+    <!-- Bootstrap -->
+    <link href="http://wb1306507.azurewebsites.net/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://wb1306507.azurewebsites.net/bootstrap-3.3.6-dist/css/extra.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+
     <title>Welcome to WanderBlog</title>
     <style type="text/css">
 		.adventure{
@@ -82,9 +101,20 @@ if(isset($_SESSION['login_user'])){
 			<img id="4" src ="http://www.aaj.tv/wp-content/uploads/2015/08/bullet_cat1.jpg" border="0" alt = "test">
         </div>
 
-        <img id="up" src = "http://i68.tinypic.com/dh7giv.jpg">
-        <img id="down" src = "http://i68.tinypic.com/2r6pq1g.jpg">
+
 	</div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8" style="border:1px solid #000;">
+            <div>
+                <img id="up" src = "http://i68.tinypic.com/dh7giv.jpg">
+                <img id="down" src = "http://i68.tinypic.com/2r6pq1g.jpg">
+            </div>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
 </div>
 </body>
 </html>
