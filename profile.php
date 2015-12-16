9c343f4e8f6a1bf622de71cb5fe907514e70c5a4
1301823 <michael.everett@outlook.com>
2015-12-16 16:59:50 +0000

    Create New Adventure

--- profile.php
+++ profile.php
@@ -33,9 +33,6 @@ if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
 ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="http://wb1306507.azurewebsites.net/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
 <nav id="navbar">
     <nav class="navbar navbar-default">
         <div class="container-fluid">
