92f02ccc63e07ac7f4aba2c15701aa648409103c
1301823 <michael.everett@outlook.com>
2015-12-16 15:07:56 +0000

    Create New Adventure

--- profiletest.php
+++ profiletest.php
@@ -28,10 +28,13 @@ include('session.php');
     <b id="logout"><a href="logout.php">Log Out</a></b>
     <b id="brian"><a href="brianslonelypage.php">All by myself</a></b>
     <?php
-    echo $_SESSION['permLevel'];
-        if($_SESSION['permLevel'] > 1){
-            showNewAdventure();
-        }
+    if($_SESSION['permLevel'] = 1){
+        echo '<b id="admin"><a href="admin.php">Click here to go to your admin page</a></b>';
+    }
+    if($_SESSION['permLevel'] > 1){
+        showNewAdventure();
+    }
+
     function showNewAdventure(){
         echo '
         <b id="admin"><a href="admin.php">Click here to go to your admin page</a></b>
