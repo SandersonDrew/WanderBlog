796d34777c6c084ef4568ca98f948f3b63a57a72
1304926 <1304926@rgu.ac.uk>
2015-12-16 14:18:23 +0000

    a

--- brianslonelypage.php
+++ brianslonelypage.php
@@ -30,6 +30,7 @@ if(isset($_SESSION['login_user'])){
 
         .adventure p{
             width: 800px;
+            height: 150px;
             text-align: justify;
             margin: 30px auto;
             overflow-y: scroll;
