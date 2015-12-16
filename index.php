daeb231925a33d4a939b3e3d1598bd8352b8ccfa
Suddenlymeatlegs <meatlegs1@gmail.com>
2015-12-16 09:55:21 +0000

    Update index.php
    Added the slideshow to main page

--- index.php
+++ index.php
@@ -8,8 +8,46 @@ if(isset($_SESSION['login_user'])){
 <head>
     <title>WanderBlog Login</title>
     <link href="style.css" rel="stylesheet" type="text/css">
+    <style type="text/css">
+		.slider{
+			width: 800px;
+			height: 350px;
+			overflow: hidden;
+			margin: 30px auto;
+		}
+
+		.slider img{
+			width:800px;
+			height:350px;
+			display: none;
+		}
+		</style>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
+		<script type="text/javascript">
+
+			function Slider(){
+				$(".slider #1").show("fade", 500);
+				$(".slider #1").delay(5500).hide("slide", {direction: 'left'},500);
+
+				var sc = $(".slider img").size();
+				var count = 2;
+
+				setInterval(function(){
+					$(".slider #"+count).show("slide",{direction: 'right'},500);
+					$(".slider #"+count).delay(5500).hide("slide",{direction: 'left'},500);
+
+					if(count == sc){
+						count = 1;
+					}
+					else{
+						count = count + 1;
+					}
+				}, 6500);
+			}
+		</script>
 </head>
-<body>
+<body onload="Slider();">
 <div id="main">
     <h1>WanderBlog Login</h1>
     <div id="login">
@@ -38,6 +76,13 @@ if(isset($_SESSION['login_user'])){
             <span><?php echo $error; ?></span>
         </form>
     </div>
+    
+    <div class = "slider">
+			<img id="1" src ="http://www.cats.org.uk/uploads/branches/211/5507692-cat-m.jpg" border="0" alt = "test">
+			<img id="2" src ="http://www.cats.org.uk/uploads/images/cats/110585_0.png" border="0" alt = "test">
+			<img id="3" src ="http://www.cats.org.uk/uploads/branches/211/adoption%20fee.png" border="0" alt = "test">
+			<img id="4" src ="http://www.aaj.tv/wp-content/uploads/2015/08/bullet_cat1.jpg" border="0" alt = "test">
+		</div>
 </div>
 </body>
 </html>
