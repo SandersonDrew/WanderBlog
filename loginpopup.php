044255c1c5696240311caa8dff7fc647ce13b957
Dan <dano.bear@hotmail.co.uk>
2015-12-16 22:01:46 +0000

    Login popup testing

--- loginpopup.php
+++ loginpopup.php
@@ -1,8 +1,8 @@
-//<?php
-//if(isset($_SESSION['login_user'])){
-//    header("location: profiletest.php");
-//}
-//?>
+<?php
+if(isset($_SESSION['login_user'])){
+    header("location: profiletest.php");
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -30,35 +30,35 @@
             <div id="popup-container">
                 <h1>Log In</h1>
 
-<!--                <div id="main">-->
-<!--                    <div id="login">-->
-<!--                        <h2>Login </h2>-->
-<!--                        <form action="login.php" method="post">-->
-<!--                            <label>UserName :</label>-->
-<!--                            <input id="name" name="username" placeholder="username" type="text">-->
-<!--                            <label>Password :</label>-->
-<!--                            <input id="password" name="password" placeholder="**********" type="password">-->
-<!--                            <input name="submit" type="submit" value=" Login ">-->
-<!--                            <span>--><?php //echo $error; ?><!--</span>-->
-<!--                        </form>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                <div id="main2">-->
-<!--                    <div id="NewUser">-->
-<!--                        <h2>New User</h2>-->
-<!--                        <form action="createNewUser.php" method="post">-->
-<!--                            <label>UserName :</label>-->
-<!--                            <input id="name" name="username" placeholder="username" type="text">-->
-<!--                            <label>Email :</label>-->
-<!--                            <input id="email" name="email" placeholder="email" type="text">-->
-<!--                            <label>Password :</label>-->
-<!--                            <input id="password" name="password" placeholder="**********" type="password">-->
-<!--                            <input name="submit" type="submit" value=" Login ">-->
-<!--                            <span>--><?php //echo $error; ?><!--</span>-->
-<!--                        </form>-->
-<!--                    </div>-->
-<!---->
-<!--                </div>-->
+                <div id="main">
+                    <div id="login">
+                        <h2>Login </h2>
+                        <form action="login.php" method="post">
+                            <label>UserName :</label>
+                            <input id="name" name="username" placeholder="username" type="text">
+                            <label>Password :</label>
+                            <input id="password" name="password" placeholder="**********" type="password">
+                            <input name="submit" type="submit" value=" Login ">
+                            <span><?php echo $error; ?></span>
+                        </form>
+                    </div>
+                </div>
+                <div id="main2">
+                    <div id="NewUser">
+                        <h2>New User</h2>
+                        <form action="createNewUser.php" method="post">
+                            <label>UserName :</label>
+                            <input id="name" name="username" placeholder="username" type="text">
+                            <label>Email :</label>
+                            <input id="email" name="email" placeholder="email" type="text">
+                            <label>Password :</label>
+                            <input id="password" name="password" placeholder="**********" type="password">
+                            <input name="submit" type="submit" value=" Login ">
+                            <span><?php echo $error; ?></span>
+                        </form>
+                    </div>
+
+                </div>
 
                 <p><a href="javascript:void(0)">Close</a> </p>
             </div>
@@ -66,7 +66,7 @@
     </div>
 
     <div id="wrapper">
-        <p><a href="javascript:void(0)">Log In</a></p>
+        <p><a href="javascript:void(0)" onclick="toggle_visibility('login-popup');">Log In</a></p>
     </div>
 
 
