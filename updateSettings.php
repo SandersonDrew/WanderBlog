387e7f063907ab594e38dab5b30dee88efe0d484
1301823 <michael.everett@outlook.com>
2015-12-16 09:49:53 +0000

    Login stuff

--- updateSettings.php
+++ updateSettings.php
@@ -1,13 +1,12 @@
 <?php
 include('session.php');
 $userid = $_SESSION['userid'];
-echo $userid;
+
 
     $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
     if ($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
     }
-    echo "Connected successfully";
     $newUsername = $_POST['name'];
     $password = $_POST['pword'];
     $email = $_POST['email'];
