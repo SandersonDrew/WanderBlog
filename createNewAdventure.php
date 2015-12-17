9079254c604ef9a79fb06ee1ef31bc0307af28d4
turkishlampost <michael.everett@outlook.com>
2015-12-17 20:38:58 +0000

    Stuff

--- createNewAdventure.php
+++ createNewAdventure.php
@@ -17,8 +17,9 @@ if (isset($_POST['submit'])) {
         $text = stripslashes($text);
         $location = stripslashes($location);
         // SQL query to insert new user details into database and log them in
+        echo $date;
         mysqli_query($connection, "INSERT INTO adventures(userid,description,location,adventurename,advdate) VALUES($userid,'$text','$location','$advname',$date) ");
         $connection->close(); // Closing Connection
-        header("location: profile.php");
+        //header("location: profile.php");
     }
 }
