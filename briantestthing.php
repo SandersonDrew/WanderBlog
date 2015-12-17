71568c6bba8a2201cf9ab95b0dbf268984c52d0e
Suddenlymeatlegs <meatlegs1@gmail.com>
2015-12-17 15:19:25 +0000

    Update briantestthing.php

--- briantestthing.php
+++ briantestthing.php
@@ -1,5 +1,4 @@
 <?php
-include('session.php');
 if (isset($_GET['submit'])) {
     $adventureid = $_GET['adventureid'];
     $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
