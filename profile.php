3bbf6b7059b51b7f1721e3a561c4f6f9687f55eb
AndrewSanderson <1306507@rgu.ac.uk>
2015-12-17 22:13:08 +0000

    Adeventure

--- profile.php
+++ profile.php
@@ -173,7 +173,7 @@ function genDivs()
     }
 
 
-$search = $_SESSION[userid];
+$sea= $_SESSION[userid];
 
 
     $conn = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
