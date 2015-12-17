1def16c679a5c4ac7e37c6f32d779d8b7b407c07
1301823 <michael.everett@outlook.com>
2015-12-17 13:52:46 +0000

    Create New Adventure

--- admin.php
+++ admin.php
@@ -144,7 +144,7 @@ function genDivs(){
         <div class="col-md-1" style="border:1px solid #000;">
         <form action="verifyUser.php" method="post">
             <input type="hidden" name="submit" value="submit"/>
-            <input name="username" value="'.$row[0].'"/>
+            <input type="image" src="http://placehold.it/60x60?text=Verify+User" name="username" value="'.$row[0].'"/>
         </form>
         </div>
         <div class="col-md-2"></div>
