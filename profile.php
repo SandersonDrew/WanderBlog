80d76064b407aba5ab8b279fae2c733382df31c0
1308353 <1308353@rgu.ac.uk>
2015-12-17 13:40:52 +0000

    Profile page edits - Body

--- profile.php
+++ profile.php
@@ -41,7 +41,7 @@ if($_GET['username'] == null){
 
 <body>
 
-<?php require_once("navbar.php"); ?>
+
 
 <?php
 $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
@@ -51,7 +51,41 @@ if ($connection->connect_error) {
 ?>
 
 
-
+<nav id="navbar">
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php"><img id="sitelogo" src="/Photos/logoback.png" height="50" width="90" alt="Logo" ></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Upload</a></li>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="admin.php">Settings</a></li>
+                    <li><a href="newAdventure.php">Create New Adventure</a></li>
+                </ul>
+                <?php
+                if($_SESSION['login_user']!= null){
+                    $name = "Logged in as " . $_SESSION['displayName'];
+                }else{
+                    $name = "Log In";
+                }
+                ?>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><?php require_once("loginpopup.php"); ?></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+</nav>
 
 
 <div class="container">
