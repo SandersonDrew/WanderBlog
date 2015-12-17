6ef999149ebf4855c4d34823149e6c4fa093e6b5
1306507 <1306507@rgu.ac.uk>
2015-12-17 18:30:22 +0000

    It is safe.

--- test.php
+++ test.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
 echo "starting";
 $sql = "SELECT * FROM adventures";
