ed78ab034915576bb84beebad479120e92e19cdb
1306507 <1306507@rgu.ac.uk>
2015-12-17 18:31:21 +0000

    It is safe.

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
 echo "starting";
 $sql = "SELECT * FROM adventures";
-$result = $mysqli->query($sql);
+$result = $connection->query($sql);
 $value = $result->fetch_array(MYSQLI_NUM);
 echo is_array($value) ? $value[0] : "";
 ?>
