93d9fa8ce0ba64e6d31a87030468d65bc0a9a3af
1308353 <1308353@rgu.ac.uk>
2015-12-17 12:23:58 +0000

    Profile page edits - Body

--- navbar.php
+++ navbar.php
@@ -45,7 +45,7 @@ if ($connection->connect_error) {
 }
 ?>
 <nav id="navbar">
-    <nav class="navbar navbar-default">
+    <nav class="navbar navbar-inverse">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
