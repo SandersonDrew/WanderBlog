f1a6e3c007a5624fe5a4464a7bf8fce77071026d
turkishlampost <michael.everett@outlook.com>
2015-12-17 21:59:31 +0000

    Stuff

--- newAdventure.php
+++ newAdventure.php
@@ -21,16 +21,64 @@ include('session.php');
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
+<body>
+<nav id="navbar">
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php"><img id="sitelogo" src="/Photos/logoback.png" height="50" width="90" alt="Logo" ></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="newAdventure.php">Upload</a></li>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="admin.php">Settings</a></li>
+                    <li><a href="newAdventure.php">Create New Adventure</a></li>
+                </ul>
+
+                <?php
+                if($_SESSION['login_user']!= null){
+                    $name = "Logged in as " . $_SESSION['displayName'];
+                }
+                ?>
+                <ul class="nav navbar-nav navbar-right">
+
+                    <li id="logged-in">
+                        <?php if($_SESSION['login_user']!= null){
+                            echo $name;
+                        }else{
+
+                            require_once("loginpopup.php");
+
+                        }
+                        ?></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+</nav>
 <div class="row">
     <div class="col-md-2"></div>
     <div class="col-md-8" style="border:1px solid #000;">
         <p><h4>Create New Adventure</h4>
         <form action="createNewAdventure.php" method="post">
+            <h6>Adventure Name:</h6><input type="text" name="advname" placeholder="Adventure Text">
             <h6>Text: </h6> <input type="text" name="Text" placeholder="Adventure Text">
-            <h6>location: </h6><input type="text" name="location" placeholder="location">
+            <h6>Location: </h6><input type="text" name="location" placeholder="Location">
+            <h6>Date: </h6><input type="text" name="date" placeholder="Date">
             <input type="submit" name="submit" value="submit">
         </form>
         <br>
     </div>
     <div class="col-md-2"></div>
 </div>
+</body>
+</html>
