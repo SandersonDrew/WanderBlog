81275c874b03148e55bf2f4824579f85447229da
1308353 <1308353@rgu.ac.uk>
2015-12-17 14:45:00 +0000

    Profile page edits - Body

--- profile.php
+++ profile.php
@@ -23,7 +23,7 @@ if($_GET['username'] == null){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/profile.css">
-    <link rel="stylesheet" type="text/css" href="</css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Profile</title>
 
