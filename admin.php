24e120c9ecc58166a6e5ae018482ba8c1dcc9775
1301823 <michael.everett@outlook.com>
2015-12-17 13:30:14 +0000

    Create New Adventure

--- admin.php
+++ admin.php
@@ -8,7 +8,7 @@ $email = $_SESSION['email'];
 $username = $_SESSION['login_user'];
 $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
 $bio = mysqli_query($connection,"SELECT bio FROM users WHERE username='$username'");
-
+echo $bio;
 ?>
 
 <!DOCTYPE html>
