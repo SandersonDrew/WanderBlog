6ad2d6ec600df03711794c1359e2e04d603d7734
1306507 <1306507@rgu.ac.uk>
2015-12-17 14:46:55 +0000

    It is safe.

--- createVote.php
+++ createVote.php
@@ -16,7 +16,7 @@ echo $swing;
 
 $sql = mysqli_query($connection, "INSERT INTO votes(userID, adventureID, swing) VALUES('$userID', '$adventureID', $swing)");
 
-if ($connection->query($sql) === TRUE) {
+if ($connection->query($sql) == TRUE) {
     echo "New records created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error .mysql_error();
