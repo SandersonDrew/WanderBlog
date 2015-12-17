7de03ac820444503c1304f28e1ca152ba199ebe1
1306507 <1306507@rgu.ac.uk>
2015-12-17 18:26:21 +0000

    It is safe.

---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+$connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
+$result = mysqli_query($connection, "SELECT * FROM adventures ");
+    ?>
