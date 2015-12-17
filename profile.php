c6943b20ba68b45ee061f28217c1e9c228f6dfe7
AndrewSanderson <1306507@rgu.ac.uk>
2015-12-17 23:17:32 +0000

    Adeventure

--- profile.php
+++ profile.php
@@ -42,6 +42,8 @@ if($_GET['username'] == null){
 
 <body>
 <?php
+
+echo TEST;
 $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
