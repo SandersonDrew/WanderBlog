933673b7b61c0952b2812c3215726a8ffc89d34a
1301823 <michael.everett@outlook.com>
2015-12-18 17:59:15 +0000

    Create New Adventure

--- editCommentHelper.php
+++ editCommentHelper.php
@@ -5,6 +5,7 @@ if (isset($_POST['submit'])) {
         // Define values
         $text = $_POST['Text'];
         $cid = $_POST['cid'];
+        $advid = $_POST['advid'];
         // Establishing Connection with Server
         $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
         mysqli_query($connection, "UPDATE comments SET text='$text'WHERE commentid = $cid");
