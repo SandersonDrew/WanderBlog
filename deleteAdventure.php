485946c3fd2b41843739a9b26c44b7b09ec6c96e
1301823 <michael.everett@outlook.com>
2015-12-18 17:01:49 +0000

    Create New Adventure

--- deleteAdventure.php
+++ deleteAdventure.php
@@ -4,3 +4,5 @@ $userid = $_SESSION['userid'];
 $advid = $_POST['advid'];
 $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
 mysqli_query($connection,"DELETE FROM adventures WHERE adventureid='$advid' AND userid='$userid'");
+$connection->close();
+header('location: profile.php');
