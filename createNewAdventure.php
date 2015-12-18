7be3b8529c2a201bf48243ece02419d75a25654a
AndrewSanderson <1306507@rgu.ac.uk>
2015-12-18 15:45:19 +0000

    Adeventure

--- createNewAdventure.php
+++ createNewAdventure.php
@@ -24,12 +24,12 @@ if (isset($_POST['submit'])) {
         }
 
         $target_dir = getcwd()."/photos/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $target_file = $target_dir . basename($_POST["fileToUpload"]["name"]);
         $uploadOk = 1;
         $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
         if(isset($_POST["submit"])) {
-            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            $check = getimagesize($_POST["fileToUpload"]["tmp_name"]);
             if($check !== false) {
                 echo "File is an image - " . $check["mime"] . ".";
                 $uploadOk = 1;
@@ -56,8 +56,8 @@ if (isset($_POST['submit'])) {
 // if everything is ok, try to upload file
         } else {
             $connection->close(); // Closing Connection
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.".$target_file;
+            if (move_uploaded_file($_POST["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "The file ". basename( $_POST["fileToUpload"]["name"]). " has been uploaded.".$target_file;
             } else {
                 echo "Sorry, there was an error uploading your file.";
             }
