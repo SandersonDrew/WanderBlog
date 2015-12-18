171fec576b5b454a8ed02927019be76cc294c267
1301823 <michael.everett@outlook.com>
2015-12-18 17:15:46 +0000

    Create New Adventure

--- index.php
+++ index.php
@@ -72,7 +72,6 @@
                     <li><a href="profile.php">Profile</a></li>
                     <li><a href="admin.php">Settings</a></li>
                 </ul>
-
                 <?php
                 if($_SESSION['login_user']!= null){
                     $name = "Logged in as " . $_SESSION['displayName'];
@@ -80,17 +79,21 @@
                 ?>
                 <ul class="nav navbar-nav navbar-right">
 
-                    <li id="logged-in">
+                    <li>
                         <?php
                         if($_SESSION['login_user']!= null){
-                            echo $name;
+                            echo "<p id=logged-in>$name</p>";
+                            ?>
+                            <button type="button" class="btn btn-info"><a href="logout.php">Log Out</a></button>
+                            <?php
                         }
                         else{
 
                             require_once("loginpopup.php");
 
                         }
-                        ?></li>
+                        ?>
+                    </li>
                 </ul>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
