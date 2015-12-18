c4622574db82d747ab71818a85e7f369371762fb
1301823 <michael.everett@outlook.com>
2015-12-18 12:36:39 +0000

    Create New Adventure

--- login.php
+++ login.php
@@ -43,7 +43,7 @@ if (isset($_POST['submit'])) {
                 $_SESSION['displayName'] = $displayName;
                 $_SESSION['permLevel'] = $permLevel;
 
-                header("location: profile.php"); // Redirecting To Other Page
+                //header("location: profile.php"); // Redirecting To Other Page
             }
         } else {
             header("location:index.php");
