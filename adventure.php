d56a5241c791ac96d896cd2a1b2eca2f065e9357
1301823 <michael.everett@outlook.com>
2015-12-18 14:24:49 +0000

    Create New Adventure

--- adventure.php
+++ adventure.php
@@ -106,15 +106,8 @@ function getval($mysqli, $sql) {
             <form action = 'createVote.php' method = "POST" >
                 <input type = "hidden" name = "userid" value = "<?php echo $userid ?>" >
                 <input type = "hidden" name = "advid" value = "<?php echo $adventureid ?>" >
-                <input type = "hidden" name = "swing" value = "1" >
-                <input type = "submit" name="submit" value="submit">
-            </form>
-            <form action = 'createVote.php' method = "POST" >
-                <p>Downvotes: </p>
-                <input type = "hidden" name = "userid" value = "<?php echo $userid ?>" >
-                <input type = "hidden" name = "advid" value = "<?php echo $adventureid ?>" >
-                <input type = "hidden" name = "swing" value = "-1" >
-                <input type = "submit" name="submit" value="submit">
+                <input type = "submit" name="submit" value="Upvote">
+                <input type = "submit" name="submit" value="Downvote">
             </form>
             <div class="col-md-2"></div>
             <div class="col-md-8">
