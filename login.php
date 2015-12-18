3ea3c48eba81a3ae887155975f53ea87a9f48b42
1301823 <michael.everett@outlook.com>
2015-12-18 12:13:34 +0000

    Create New Adventure

--- login.php
+++ login.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start(); // Starting Session
 $error=''; // Variable To Store Error Message
 if (isset($_POST['submit'])) {
