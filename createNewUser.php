da6bf95f62bfdf3a4598da30c764027817755f58
1301823 <michael.everett@outlook.com>
2015-12-18 10:00:15 +0000

    Create New Adventure

--- createNewUser.php
+++ createNewUser.php
@@ -11,6 +11,7 @@ if (isset($_POST['submit'])) {
         $username=$_POST['username'];
         $password=$_POST['password'];
         $email = $_POST['email'];
+
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
 // Selecting Database
         $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
