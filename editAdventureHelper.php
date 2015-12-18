a695660e90dbc96a5e48f6556a14ea2b6496f25e
1301823 <michael.everett@outlook.com>
2015-12-18 15:59:46 +0000

    Create New Adventure

--- editAdventureHelper.php
+++ editAdventureHelper.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include('session.php');
 if (isset($_POST['submit'])) {
     if (!empty($_POST['Text']) && !empty($_POST['Location']) && !empty($_POST['advname']) && !empty($_POST['date'])) {
@@ -12,6 +15,6 @@ if (isset($_POST['submit'])) {
         // Establishing Connection with Server
         $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
         mysqli_query($connection, "UPDATE adventures SET  adventurename=" . $name . " ,description=" . $text . " ,advdate=" . $date . " ,location=" . $location . "WHERE adventureid=" . $advid);
-        header("location: profile.php");
+        //header("location: profile.php");
     }
 }
