88ebd28d5545f8aed1190073f1ced807de5c7b4f
AndrewSanderson <1306507@rgu.ac.uk>
2015-12-18 15:27:32 +0000

    Adeventure

--- upload.php
+++ upload.php
@@ -4,7 +4,7 @@ if($_get['username']==null){
     $userid = $_getSession['userid'];
 }
 include('session.php');
-$target_dir = getcwd()."/photos/".userid."//";
+$target_dir = getcwd()."/photos/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
