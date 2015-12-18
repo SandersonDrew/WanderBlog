4fecc43dd59a56f78b04e1ae92bf9922dae89f2d
1301823 <michael.everett@outlook.com>
2015-12-18 15:58:21 +0000

    Create New Adventure

--- editAdventureHelper.php
+++ editAdventureHelper.php
@@ -1,10 +1,7 @@
 <?php
 include('session.php');
 if (isset($_POST['submit'])) {
-    if (empty($_POST['Text']) || empty($_POST['Location'])) {
-        $error = "Text or Location is empty";
-        echo $error;
-    } else {
+    if (!empty($_POST['Text']) && !empty($_POST['Location']) && !empty($_POST['advname']) && !empty($_POST['date'])) {
         // Define values
         $advid = $_POST['advid'];
         $name = $_POST['advname'];
@@ -14,7 +11,7 @@ if (isset($_POST['submit'])) {
         $userid = $_SESSION['userid'];
         // Establishing Connection with Server
         $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
-        mysqli_query($connection,"UPDATE adventures SET  adventurename=".$name." description=".$text." advdate=".$date."WHERE adventureid=".$advid);
+        mysqli_query($connection, "UPDATE adventures SET  adventurename=" . $name . " ,description=" . $text . " ,advdate=" . $date . " ,location=" . $location . "WHERE adventureid=" . $advid);
         header("location: profile.php");
     }
 }
