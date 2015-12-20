301752007c60e04470f93f52ede3955be2af15eb
AndrewSanderson <1306507@rgu.ac.uk>
2015-12-20 00:20:01 +0000

    Adeventure

--- createNewAdventure.php
+++ createNewAdventure.php
@@ -13,10 +13,7 @@ if (isset($_POST['submit'])) {
         $location = $_POST['Location'];
         $date = $_POST['date'];
         $userid = $_SESSION['userid'];
-        $target_dir = getcwd()."/photos/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-        $uploadOk = 1;
+
         // Establishing Connection with Server
         $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
         // To protect MySQL injection for Security purpose
@@ -25,15 +22,18 @@ if (isset($_POST['submit'])) {
 
         // SQL query to insert new user details into database and log them in
         mysqli_query($connection, "INSERT INTO adventures(userid,description,location,adventurename,advdate) VALUES($userid,'$text','$location','$name','$date') ");
-
-// Check if image file is a actual image or fake image]
+        $target_dir = getcwd() . "/photos/";
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+        if (isset($_POST["submit"])) {
             $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-            if($check !== false) {
+            if ($check !== false) {
                 echo "File is an image - " . $check["mime"] . ".";
                 $uploadOk = 1;
             } else {
-                echo $imageFileType;
-                echo "File is not an imagez.";
+                echo "File is not an image.";
                 $uploadOk = 0;
             }
         }
@@ -44,21 +44,24 @@ if (isset($_POST['submit'])) {
         }
 // Check fil
 // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif"
+        ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
 // Check if $uploadOk is set to 0 by an error
         if ($uploadOk == 0) {
             echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
         } else {
-            $connection->close(); // Closing Connection
-            if (move_uploaded_file($_POST["fileToUpload"]["tmp_name"], $target_file)) {
-                echo "The file ". basename( $_POST["fileToUpload"]["name"]). " has been uploaded.".$target_file;
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded." . $target_file;
             } else {
                 echo "Sorry, there was an error uploading your file.";
             }
         }
+
     }
+}
+?>
