5520309a473dc4957c9516b9966413fa94f1a7c9
AndrewSanderson <1306507@rgu.ac.uk>
2015-12-21 01:12:09 +0000

    Adeventure

--- upvote.php
+++ upvote.php
@@ -6,5 +6,6 @@ if (isset($_POST['submit'])) {
         $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
         $swing = $_POST['swing'];
         mysqli_query($connection, "INSERT INTO votes(adventureid,userid,swing) VALUES($adventureid,$userid,$swing) ");
+        echo $adventureid;
         $connection->close(); // Closing Connection
 }
