7d9163d649f44e8ffc7bbfdf7c34b4621244332e
AndrewSanderson <1306507@rgu.ac.uk>
2015-12-21 02:36:51 +0000

    Adeventure

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ include('session.php');
 <?php
 
 $connection = new mysqli("eu-cdbr-azure-west-c.cloudapp.net", "b0b05a48637b3e", "2d0628d7", "wb1306507");
-$result = mysqli_query($connection, "SELECT * FROM adventures");
+$result = mysqli_query($connection, "SELECT * FROM adventures LIMIT 5");
 
 
 if ($result->num_rows > 0) {
