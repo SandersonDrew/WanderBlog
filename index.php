a21d3a93f8f630b436efffa618c7687d4f20759a
AndrewSanderson <1306507@rgu.ac.uk>
2015-12-21 03:18:39 +0000

    Adeventure

--- index.php
+++ index.php
@@ -135,7 +135,7 @@ if ($result->num_rows > 0) {
 
     while ($row = $result->fetch_assoc()) {
     $search_dir = getcwd()."/photos/".$row{'adventureid'}."/";
-    $images = glob("$search_dir");
+    $images = glob($search_dir."*");
 
     // Image selection and display:
 
